2d56a636ab765e75725dd5569dab34f3054bf7f5
exy <teslawiktor@gmail.com>
2024-12-27 20:40:52 +0100

    1h:13

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* Text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br /> <span className="text-accent">Wiktor Kowalczyk</span>
@@ -37,7 +37,7 @@ const Home = () => {
             </div>
           </div>
           {/* Photo */}
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
