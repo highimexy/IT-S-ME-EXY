39f318924176d975ac19410bfac77191489042d5
exy <teslawiktor@gmail.com>
2024-12-27 17:16:07 +0100

    58:02

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,38 @@
-import { Button } from "@/components/ui/button"
-import { FiDownload} from "react-icons/fi"
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
 const Home = () => {
-  return <section className="h-full">
-    <div className="container mx-auto h-full">
-      <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-        {/*Text*/}
-        <div className="text-center xl:text-left">
-          <span className="text-xl">Software Developer</span>
-          <h1 className="h1 mb-6">
-            Hello I'm <br /> <span className="text-accent">Wiktor Kowalczyk</span>
-          </h1>
-          <p className="max-w-[500px] mb-9 text-white/80">
-            I excel at crafting elegant digital experiences and 
-            I am proficient in various programming languages and technologies.
-          </p>
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* Text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br /> <span className="text-accent">Wiktor Kowalczyk</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button 
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div>Socials</div>
+            </div>
+          </div>
+          {/* Photo */}
+          <div>photo</div>
         </div>
-        {/*Photo*/}
-        <div>photo</div>
       </div>
-    </div>
-  </section>
-  
-}
+    </section>
+  );
+};
 
-export default Home
+export default Home;
