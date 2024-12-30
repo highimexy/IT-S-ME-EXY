a9ae435e97e82eb46d05ec142bf08ff8458ab765
exy <teslawiktor@gmail.com>
2024-12-30 09:45:16 +0100

    import

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
 const Work = () => {
-    return (
-      <div>work page</div>
-    )
-  };
-  
-  export default Work;
+  return <div>work page</div>;
+};
+
+export default Work;
