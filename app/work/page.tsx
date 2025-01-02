a5d9de6c216d4213cdef4bc6db5c33b96ce5fa45
exy <teslawiktor@gmail.com>
2025-01-02 21:02:20 +0100

    2:30:28

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -54,8 +54,17 @@ const projects = [
   },
 ];
 
+
 const Work = () => {
   const [project, setProject] = useState(projects[0]);
+
+  const handleSlideChange = (swiper) => {
+    //get current slide index
+    const currentIndex = swiper.activeIndex;
+    //update project state based on current slide index
+    setProject(projects[currentIndex]);
+  }
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
@@ -79,9 +88,13 @@ const Work = () => {
               {/* STACK */}
               <ul className="flex gap-4">
                 {project.stack.map((item, index) => {
-                  return <li key={index} className="font-xl text-accent">{item.name}
-                  {/* REMOVE THE LAST COMMA */}
-                  {index !== project.stack.length - 1 && ","}</li>
+                  return (
+                    <li key={index} className="font-xl text-accent">
+                      {item.name}
+                      {/* REMOVE THE LAST COMMA */}
+                      {index !== project.stack.length - 1 && ","}
+                    </li>
+                  );
                 })}
               </ul>
               {/* BORDER */}
@@ -119,7 +132,18 @@ const Work = () => {
               </div>
             </div>
           </div>
-          <div className="w-full xl:w-[50%]">slider</div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return <SwiperSlide key={index}>slide</SwiperSlide>;
+              })}
+            </Swiper>
+          </div>
         </div>
       </div>
     </motion.section>
