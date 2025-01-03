53917dd7cf1917891d55d9143e5ea142bb40c931
exy <teslawiktor@gmail.com>
2025-01-03 08:51:50 +0100

    const info, all imports

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,43 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+48) 502 329 XXX",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "teslawiktor@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    description: "Gdańsk",
+  },
+]
+
+import { motion } from "framer-motion";
+
 const Contact = () => {
-    return (
-      <div>contactt page</div>
-    )
-  };
-  
-  export default Contact;
+  return <div>contactt page</div>;
+};
+
+export default Contact;
