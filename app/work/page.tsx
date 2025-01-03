1e2ea675f8e36989cc0ea208ce579caeb54e2a7d
exy <teslawiktor@gmail.com>
2025-01-03 08:27:41 +0100

    2h:39:50

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -17,6 +17,7 @@ import {
 
 import Link from "next/link";
 import Image from "next/image";
+import WorkSliderBtns from "@/components/WorkSliderBtns";
 
 const projects = [
   {
@@ -160,6 +161,11 @@ const Work = () => {
                   </SwiperSlide>
                 );
               })}
+              {/* slider buttons */}
+              <WorkSliderBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[30px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+              />
             </Swiper>
           </div>
         </div>
